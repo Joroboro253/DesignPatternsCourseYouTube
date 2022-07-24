fe09170e193acc97e3b8f17c886aa226301011cd
Joroboro253 <vlyashchenko01@gmail.com>
2022-07-24 22:14:37 +0300

    works

---
+++ src/proselyte/basepatterns/behavioral/chain/SimpleReportNotifier.java
@@ -0,0 +1,15 @@
+package proselyte.basepatterns.behavioral.chain;
+
+import proselyte.basepatterns.behavioral.chain.Notifier;
+
+public class SimpleReportNotifier extends Notifier {
+
+    public SimpleReportNotifier(int priority) {
+        super(priority);
+    }
+
+    @Override
+    public void write(String message) {
+        System.out.println("Notifying using simple report: " + message);
+    }
+}
---
+++ src/proselyte/basepatterns/behavioral/template/NewsPage.java
@@ -0,0 +1,9 @@
+package proselyte.basepatterns.behavioral.template;
+
+public class NewsPage extends  WebsiteTemplate{
+    @Override
+    public void showPageContent() {
+        System.out.println("News");
+    }
+
+}
